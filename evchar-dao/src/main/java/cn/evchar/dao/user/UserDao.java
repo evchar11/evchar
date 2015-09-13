4b16ea34ff5046839266c55308b51b0a72b92be8
tianguangnan <tianguangnan@evchar.cn>
2015-09-13 16:48:29 +0800

    ahua

--- evchar-dao/src/main/java/cn/evchar/dao/user/UserDao.java
+++ evchar-dao/src/main/java/cn/evchar/dao/user/UserDao.java
@@ -8,18 +8,17 @@ import cn.evchar.common.util.StringUtils;
 import cn.evchar.dao.AbstractBaseDao;
 
 @Repository
-public class UserDao extends AbstractBaseDao<User, Long>{
-    public static final String GET_BY_NICKNAME_HQL = "from User where nickName = ?" ;
-    public static final String GET_BY_WECHATID_HQL = "from User where wechatId = ?" ;
-    public User getByNickName(String nickName){
-        Assert.isTrue(StringUtils.isNotBlank(nickName));
-        return unique(GET_BY_NICKNAME_HQL, nickName);
-    }
-    
-    public User getByWechatId(String wechatId){
-        Assert.isTrue(StringUtils.isNotBlank(wechatId));
-        return unique(GET_BY_WECHATID_HQL, wechatId);
-    }
+public class UserDao extends AbstractBaseDao<User, Long> {
+	public static final String GET_BY_NICKNAME_HQL = "from User where nickName = ?";
+	public static final String GET_BY_WECHATID_HQL = "from User where wechatId = ?";
+	public User getByNickName(String nickName) {
+		Assert.isTrue(StringUtils.isNotBlank(nickName));
+		return unique(GET_BY_NICKNAME_HQL, nickName);
+	}
 
+	public User getByWechatId(String wechatId) {
+		Assert.isTrue(StringUtils.isNotBlank(wechatId));
+		return unique(GET_BY_WECHATID_HQL, wechatId);
+	}
 
 }
