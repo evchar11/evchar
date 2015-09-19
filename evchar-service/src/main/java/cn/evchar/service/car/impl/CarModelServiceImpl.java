f942e3370b0d5fe84c7d10c596c96fc21ced791a
mrz <mrz@evchar.cn>
2015-09-19 20:56:17 +0800

    返回车的具体型号时带上该型号主键

--- evchar-service/src/main/java/cn/evchar/service/car/impl/CarModelServiceImpl.java
+++ evchar-service/src/main/java/cn/evchar/service/car/impl/CarModelServiceImpl.java
@@ -10,12 +10,15 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import cn.evchar.common.entity.car.CarModel;
 import cn.evchar.dao.car.CarModelDao;
 import cn.evchar.service.car.ICarModelService;
 
 @Service
 public class CarModelServiceImpl implements ICarModelService {
+	private static final String CAR_MODEL_SPLITER = "_";
 
 	@Resource
 	private CarModelDao carModelDao;
@@ -41,9 +44,13 @@ public class CarModelServiceImpl implements ICarModelService {
 		carModel.setBrand(brand);
 		List<CarModel> modelList = carModelDao.findByExample(CarModel.class,
 				carModel);
+		StringBuilder builder;
 		List<String> subModelList = new ArrayList<>();
 		for (CarModel model : modelList) {
-			subModelList.add(model.getModel());
+			builder = new StringBuilder();
+			builder.append(model.getModel()).append(CAR_MODEL_SPLITER)
+					.append(model.getId());
+			subModelList.add(builder.toString());
 		}
 		return subModelList;
 	}
