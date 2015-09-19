c853caf7d44daf6f9944274aef71021e5109d2f6
mrz <mrz@evchar.cn>
2015-09-19 20:27:01 +0800

    添加设备状态接口方法

--- evchar-device/src/main/java/cn/evchar/device/hardware/DeviceStateListener.java
+++ evchar-device/src/main/java/cn/evchar/device/hardware/DeviceStateListener.java
@@ -1,5 +1,28 @@
 package cn.evchar.device.hardware;
 
 public interface DeviceStateListener {
-	
+	/**
+	 * 上电
+	 */
+	void onEnergized(Long deviceId);
+
+	/**
+	 * 开始充电
+	 */
+	void onStartCharging(Long deviceId);
+
+	/**
+	 * 结束充电
+	 */
+	void onEndCharging(Long deviceId);
+
+	/**
+	 * 充满
+	 */
+	void onFull(Long deviceId);
+
+	/**
+	 * 电量度数变化
+	 */
+	void onDegreeChange(Long deviceId, Long degree);
 }
---
+++ evchar-device/src/main/java/cn/evchar/device/hardware/protocol/types/DeviceStateType.java
@@ -0,0 +1,31 @@
+package cn.evchar.device.hardware.protocol.types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum DeviceStateType {
+	// 空闲/已预约/已上电/正在充电/已充满
+	IDLE("01"), RESERVED("02"), ENERGIZED("03"), CHARGING("04"), FULL("05");
+
+	private final String str;
+
+	DeviceStateType(String str) {
+		this.str = str;
+	}
+
+	private static final Map<String, DeviceStateType> stringToEnum = new HashMap<String, DeviceStateType>();
+
+	static {
+		for (DeviceStateType op : values())
+			stringToEnum.put(op.toString(), op);
+	}
+
+	public static DeviceStateType fromString(String symbol) {
+		return stringToEnum.get(symbol);
+	}
+
+	@Override
+	public String toString() {
+		return str;
+	}
+}
